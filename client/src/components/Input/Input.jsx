9640d43d853f5fcdb2fea3295a375127fffc89ce
Jao <jp871157@gmail.com>
2026-04-13 22:33:40 -0300

    arrumei uns trem ai

--- client/src/components/Input/Input.jsx
+++ client/src/components/Input/Input.jsx
@@ -4,6 +4,33 @@ import './input.css'
 export const Input = ({ id, placeholder, inputType, LabelIcon, hasIconAside }) => {
     const [dirty, setDirty] = useState(false);
 
+    const handleInputEvent = (e) => {
+        if(inputType != 'number') { // If the input is not a number, do nothing but set dirty to true or false.
+            setDirty(e.target.value.length > 0)
+            return
+        }
+        
+        // Otherwise, needs to normalize the number. Since input type="number" won't accept floating point numbers, had to do one by hand (if u're reading this, help me pls I HATE JAVASCRIPT).
+        const val = e.target.value
+        const rmvLetters = val.replace(/[^\d.,]/g, '').replace(',', '.')
+
+        // Just in case user tries to input more than one "." for decimal part.
+        let hasDecimal = false
+        let normNum = ''
+        for(const digit of rmvLetters) {
+            if(digit == '.' && hasDecimal) {
+                continue
+            } else if(digit == '.') {
+                hasDecimal = true;
+            }
+
+            normNum += digit
+        }
+        
+        document.querySelector(`input#${id}`).value = normNum
+        setDirty(e.target.value.length > 0)
+    }
+
     return (
         <div className='input_container'>
             {
@@ -12,7 +39,7 @@ export const Input = ({ id, placeholder, inputType, LabelIcon, hasIconAside }) =
                     <LabelIcon />
                 </label> : null
             }
-            <input onInput={(e) => setDirty(e.target.value.length > 0)} type={inputType} id={id} />
+            <input onInput={(e) => handleInputEvent(e)} type={inputType != 'number' ? inputType : 'text'} id={id} />
             <label htmlFor={id} className={`placeholder font_inter_semibold ${dirty ? "active" : ""}`}>{placeholder}</label>
         </div>
     )
