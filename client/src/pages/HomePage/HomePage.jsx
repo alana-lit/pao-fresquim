3f6b047da2b34ffbcf995dccb9a2347d286ff8aa
Jao <jp871157@gmail.com>
2026-05-01 23:33:50 -0300

    SEU GUARDA EU NAO SOU VAGABUNDO EU NAO SOU DELINQUENTE SOU SÓ UM CARA CARENTE, EU DORMI NA PRAAAAAAAÇAAAAAAAAAAAAAAAAAAAAA PENSANDO NELAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -9,12 +9,8 @@ export const HomePage = () => {
             <section>
                 <h1 className="font_poppins_bold">Bem vindo(a), João!</h1>
                 <p className="font_inter_regular">
-                    Eu não SOU o perigo, sr QUIM. Eu ESTOU em perigo!<br/>
-                    Um cliente compra pão de ontem e você acha que eu estou com problemas?<br/>
-                    Você tá totalmente certo! EU sou demitido; Eu DESAPAREÇO; Eu DEIXO DE EXISTIR!
+                    Mais frescos que os nossos pães? - Só o seu Joaquim!
                 </p>
-                { /* https://youtu.be/Ly1o219rj5U?si=G6JNP8Pz2KU79yp7 -> original */ }
-                { /* https://www.youtube.com/watch?v=bI8Iy7dHlME -> MEME */ }
             </section>
         </div>
     )
