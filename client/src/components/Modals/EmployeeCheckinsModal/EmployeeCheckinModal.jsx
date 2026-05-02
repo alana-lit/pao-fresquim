ebb2e0fec0edbdb3496806afe6fb2965848163b1
Jao <jp871157@gmail.com>
2026-05-02 00:02:34 -0300

    concerto ou conserto, eis a questao

--- client/src/components/Modals/EmployeeCheckinsModal/EmployeeCheckinModal.jsx
+++ client/src/components/Modals/EmployeeCheckinsModal/EmployeeCheckinModal.jsx
@@ -1,12 +1,18 @@
+import { CiCalendarDate } from "react-icons/ci"
+
 import { checkinMock } from "../../../mocks/employeeCheckinsMock"
 import { CheckinCard } from "./CheckinCard/CheckinCard"
+import { Input } from "../../Input/Input"
 
 import './employeeCheckinModal.css'
 
 export const EmployeeCheckinModal = ({ employeeId }) => {
     return (
-        <ul className="scrollbar checkin_container">
-            {checkinMock.map((info, idx) => <CheckinCard info={{...info, 'type': idx % 2 == 0 ? "Entrada" : "Saída"}} key={idx}/>)}
-        </ul>
+        <div className="checkin_container">
+            <ul className="scrollbar ">
+                {checkinMock.map((info, idx) => <CheckinCard info={{...info, 'type': idx % 2 == 0 ? "Entrada" : "Saída"}} key={idx}/>)}
+            </ul>
+            <Input LabelIcon={CiCalendarDate} hasIconAside={true} inputType="date" id="dateFilter" />
+        </div>
     )
 }
