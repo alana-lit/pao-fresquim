12eb1e436f64078fc3e8be3d02e9faf8e9c40e4d
Jao <jp871157@gmail.com>
2026-05-02 23:16:37 -0300

    ELA PARTIIIIIIIIIIIIIIIIIIIIIIIIIIIIU PARTIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIUU E NUNCA MAIS VOLTOOOOOOOOOOOOOOOOOOOOOOOOOOOO

--- client/src/components/Modals/EmployeeAddLicenseModal/EmployeeAddLicenseModal.jsx
+++ client/src/components/Modals/EmployeeAddLicenseModal/EmployeeAddLicenseModal.jsx
@@ -2,14 +2,36 @@ import { CiImageOn } from "react-icons/ci"
 import { Input } from "../../Input/Input"
 
 import './employeeAddLicenseModal.css'
+import { useState } from "react"
 
 export const EmployeeAddLicenseModal = ({ employeeId }) => {
+    const [fileName, setFileName] = useState("Escolher arquivo")
+    
+    const handleSubmit = async (e) => {
+        const _toBase64 = (file) => {
+            return new Promise((resolve, reject) => {
+                const reader = new FileReader()
+    
+                reader.readAsDataURL(file)
+    
+                reader.onload = () => resolve(reader.result)
+                reader.onerror = reject
+            })
+        }
+        e.preventDefault()
+        
+        const file = e.target[0].files[0]
+
+        const base64 = await _toBase64(file)
+        console.log(base64)
+    }
+
     return (
-        <form className="add_license_form">
-            <input id="addLicenseInput" type="file" hidden />
+        <form className="add_license_form" onSubmit={handleSubmit}>
+            <input id="addLicenseInput" type="file" hidden onChange={(e) => setFileName(e.target.value)} />
             <label htmlFor="addLicenseInput" className="font_inter_regular">
                 <CiImageOn />
-                Escolher arquivo
+                {fileName}
             </label>
             <button className="font_inter_semibold">Registrar atestado</button>
         </form>
